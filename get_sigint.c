2ee625e223db3839f42d2c8aa3a58f572065a9bb
Nada <nadaait171@gmail.com>
2023-05-18 15:42:00 -0700

    sigint

---
+++ get_sigint.c
@@ -0,0 +1,12 @@
+#include "shell.h"
+
+/**
+ * get_sigint - handle the ctrl + c call in prompt
+ * @sig: signal handler
+ */
+
+void get_sigint(int sig)
+{
+	(void)sig;
+	write(STDOUT_FILENO, "\n^-^ ", 5);
+}
